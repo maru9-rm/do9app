2fccea50f6f7e255fdcdd045f5b146a5b351d7b7
Ryota9 <ngm9dwf@gmail.com>
2021-03-15 22:47:27 +0900

    新しいタスクを作る。アイキャッチの設定。途中だが一旦ボード詳細画面作るため中断上げ

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -6,4 +6,21 @@ class TasksController < ApplicationController
     def new
         @task = current_user.tasks.build
     end
+
+    def create
+        @task = current_user.tasks.build(task_params)
+        if @task.save
+            redirect_to task_path(@task), notice: 'Saved in successfully'
+        else
+            flash.now[:error] = 'Failed to save'
+            render :new
+        end
+    end
+
+
+
+    private
+    def task_params
+        params.require(:task).permit(:title, :content, :eyecatch)
+    end
 end
--- app/models/task.rb
+++ app/models/task.rb
@@ -2,4 +2,5 @@ class Task < ApplicationRecord
     belongs_to :user, optional: true
     validates :title, presence: true
     validates :content, presence: true
+    has_one_attached :eyecatch
 end
