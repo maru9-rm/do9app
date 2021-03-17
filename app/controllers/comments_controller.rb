4d206f4057031b9dd98fec9c83350601cfa18f05
Ryota9 <ngm9dwf@gmail.com>
2021-03-18 00:23:51 +0900

    コメント一覧が見えるようになった

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,22 @@
 class CommentsController < ApplicationController
+    def new
+        @comment = current_user.comments.build
+    end
+
+    def create
+        @comment = current_user.comments.build(comment_params)
+        if @comment.save
+            redirect_to root_path, notice: 'Saved in successfully'
+        else
+            flash.now[:error] = 'Failed to save'
+            render :new
+        end
+    end
+
+
+    private
+    def comment_params
+        params.require(:comment).permit(:content)
+    end
+    
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   
   resources :boards
   resources :tasks
+  resources :comments
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
